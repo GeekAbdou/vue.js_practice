9a6678c5042791d30058bd06c25b4538ce3d3a00
Abd-Elrahman Elsayed <abd.hassan98@eng-st.cu.edu.eg>
2021-10-10 05:56:52 +0200

    vue events app

---
+++ Second-App/js/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+
+  methods: {
+    add() {
+      this.counter = this.counter + 1;
+    },
+
+    reduce() {
+      this.counter = counter - 1;
+    },
+  },
+  
+});
+
+app.mount("#app");
